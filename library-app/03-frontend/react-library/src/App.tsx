72c84f73620e0b14ba3fdfdbbf9aa799aec785ee
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-01-24 22:03:44 +0200

    Create Search Books

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Footer } from './layouts/HomePage/components/Footer';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { SearchBooksPage } from './layouts/SearchBookPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
       <Navbar/>
-        <HomePage/>
+        {/*<HomePage/>*/}
+        <SearchBooksPage/>
       <Footer/>
     </div>
   );
